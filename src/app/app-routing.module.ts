973ef8c8b3eba478753fe8aa9116d4cd8a0b5b71
yangjie <261417807@qq.com>
2023-07-06 10:32:14 +0800

    'test'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,11 +8,11 @@ const routes: Routes = [
   },
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
+    loadChildren: () => import('./pages/home/home.module').then(m => m.HomePageModule)
   },
   {
     path: 'message/:id',
-    loadChildren: () => import('./view-message/view-message.module').then(m => m.ViewMessagePageModule)
+    loadChildren: () => import('./pages/view-message/view-message.module').then(m => m.ViewMessagePageModule)
   }
 ];
 
